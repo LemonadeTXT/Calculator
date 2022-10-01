4aef2875759267bfe6c5c3225b3af1d4b10492b0
Stecko Anton <lemonadetxt@gmail.com>
2022-10-01 18:33:55 +0300

    create wpf project

---
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,286 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool _lastElementIsDigit;
+        bool _lastOperationIsDecimalPoint;
+        readonly List<char> _listOfOperations;
+
+        public MainWindow()
+        {
+            _listOfOperations = new List<char>() { '+', '-', '*', '÷' };
+
+            InitializeComponent();
+        }
+
+        private void ButtonZero_Click(object sender, RoutedEventArgs e)
+        {
+            var stringToCalculate = textDisplay.Content.ToString();
+
+            if (textDisplay.Content.ToString().Length < 3)
+            {
+                if (!(StringToCalculateIsZero(stringToCalculate) || StringToCalculateIsMinusZero(stringToCalculate)))
+                {
+                    textDisplay.Content += "0";
+
+                    _lastElementIsDigit = true;
+                }
+            }
+            else
+            {
+                if (!IsZeroAfterOperation(stringToCalculate))
+                {
+                    textDisplay.Content += "0";
+
+                    _lastElementIsDigit = true;
+                }
+            }
+        }
+
+        private void ButtonOne_Click(object sender, RoutedEventArgs e)
+        {
+            InputDigit("1");
+        }
+
+        private void ButtonTwo_Click(object sender, RoutedEventArgs e)
+        {
+            InputDigit("2");
+        }
+
+        private void ButtonThree_Click(object sender, RoutedEventArgs e)
+        {
+            InputDigit("3");
+        }
+
+        private void ButtonFour_Click(object sender, RoutedEventArgs e)
+        {
+            InputDigit("4");
+        }
+
+        private void ButtonFive_Click(object sender, RoutedEventArgs e)
+        {
+            InputDigit("5");
+        }
+
+        private void ButtonSix_Click(object sender, RoutedEventArgs e)
+        {
+            InputDigit("6");
+        }
+
+        private void ButtonSeven_Click(object sender, RoutedEventArgs e)
+        {
+            InputDigit("7");
+        }
+
+        private void ButtonEight_Click(object sender, RoutedEventArgs e)
+        {
+            InputDigit("8");
+        }
+
+        private void ButtonNine_Click(object sender, RoutedEventArgs e)
+        {
+            InputDigit("9");
+        }
+
+        private void ButtonDecimalPoint_Click(object sender, RoutedEventArgs e)
+        {
+            if (_lastElementIsDigit && !_lastOperationIsDecimalPoint)
+            {
+                textDisplay.Content += ",";
+
+                _lastElementIsDigit = false;
+                _lastOperationIsDecimalPoint = true;
+            }
+        }
+
+        private void ButtonDivide_Click(object sender, RoutedEventArgs e)
+        {
+            InputOperation("÷");
+        }
+
+        private void ButtonMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            InputOperation("*");
+        }
+
+        private void ButtonSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            if (textDisplay.Content.ToString() == string.Empty)
+            {
+                textDisplay.Content = "-";
+
+                _lastElementIsDigit = false;
+                _lastOperationIsDecimalPoint = false;
+            }
+            else
+            {
+                InputOperation("-");
+            }
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            InputOperation("+");
+        }
+
+        private void ButtonClean_Click(object sender, RoutedEventArgs e)
+        {
+            textDisplay.Content = string.Empty;
+
+            _lastElementIsDigit = false;
+        }
+
+        private void ButtonResult_Click(object sender, RoutedEventArgs e)
+        {
+            var stringToCalculate = textDisplay.Content.ToString();
+
+            if (IsCorrectStringToCalculate(stringToCalculate))
+            {
+                stringToCalculate = stringToCalculate.Replace('÷', '/').Replace(',', '.');
+
+                object result = new DataTable().Compute(stringToCalculate, null);
+
+                if (result.ToString().Contains("."))
+                {
+                    _lastOperationIsDecimalPoint = true;
+                }
+
+                textDisplay.Content = result.ToString().Replace('.', ',');
+            }
+        }
+
+        private void InputDigit(string digit)
+        {
+            var stringToCalculate = textDisplay.Content.ToString();
+
+            if (stringToCalculate.Length < 3)
+            {
+                if (StringToCalculateIsZero(stringToCalculate))
+                {
+                    textDisplay.Content = digit;
+                }
+                else if (StringToCalculateIsMinusZero(stringToCalculate))
+                {
+                    textDisplay.Content = "-" + digit;
+                }
+                else
+                {
+                    textDisplay.Content += digit;
+                }
+            }
+            else if (IsZeroAfterOperation(stringToCalculate))
+            {
+                ReplaceLastZero(stringToCalculate, digit);
+            }
+            else
+            {
+                textDisplay.Content += digit;
+            }
+
+            _lastElementIsDigit = true;
+        }
+
+        private void InputOperation(string operation)
+        {
+            if (_lastElementIsDigit)
+            {
+                textDisplay.Content += operation;
+
+                _lastElementIsDigit = false;
+                _lastOperationIsDecimalPoint = false;
+            }
+        }
+
+        public bool StringToCalculateIsZero(string stringToCalculate)
+        {
+            if (stringToCalculate is "0")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool StringToCalculateIsMinusZero(string stringToCalculate)
+        {
+            if (stringToCalculate is "-0")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool IsZeroAfterOperation(string stringToCalculate)
+        {
+            if (stringToCalculate.Length > 2)
+            {
+                var lastDigit = stringToCalculate[stringToCalculate.Length - 1];
+                var preLastDigit = stringToCalculate[stringToCalculate.Length - 2];
+
+                if (_listOfOperations.Contains(preLastDigit) && lastDigit is '0')
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void ReplaceLastZero(string stringToCalculate, string digit)
+        {
+            stringToCalculate = stringToCalculate.Remove(stringToCalculate.Length - 1);
+
+            textDisplay.Content = stringToCalculate.Insert(stringToCalculate.Length, digit);
+        }
+
+        public bool IsCorrectStringToCalculate(string stringToCalculate)
+        {
+            if (string.IsNullOrEmpty(stringToCalculate))
+            {
+                MessageBox.Show("String to calculate is empty!");
+
+                return false;
+            }
+            else if (!char.IsDigit(stringToCalculate[stringToCalculate.Length - 1]))
+            {
+                MessageBox.Show("The calculation string was entered incorrectly (perhaps the last character is not a number)!");
+
+                return false;
+            }
+
+            bool correctString = false;
+
+            if (stringToCalculate[0] is '-')
+            {
+                stringToCalculate = stringToCalculate.Remove(0, 1);
+            }
+
+            foreach (var item in _listOfOperations)
+            {
+                if (stringToCalculate.Contains(item))
+                {
+                    correctString = true;
+                    break;
+                }
+            }
+
+            if (!correctString)
+            {
+                MessageBox.Show("Please do some operatons!");
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Calculator/CalculatorTests/MainWindowTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        readonly MainWindow calculator = new MainWindow();
+
+        [TestMethod()]
+        public void IsCorrectStringToCalculate_EmptyString_False()
+        {
+            Assert.AreEqual(false, calculator.IsCorrectStringToCalculate(string.Empty));
+        }
+
+        [TestMethod()]
+        public void IsCorrectStringToCalculate_OneDecimalPonit_False()
+        {
+            Assert.AreEqual(false, calculator.IsCorrectStringToCalculate("1."));
+        }
+
+        [TestMethod()]
+        public void IsCorrectStringToCalculate_FivePlusSevenMinus_False()
+        {
+            Assert.AreEqual(false, calculator.IsCorrectStringToCalculate("5+7-"));
+        }
+
+        [TestMethod()]
+        public void IsCorrectStringToCalculate_LongStringToCalculate_False()
+        {
+            Assert.AreEqual(false, calculator.IsCorrectStringToCalculate("96+2.39-78875/"));
+        }
+
+        [TestMethod()]
+        public void IsCorrectStringToCalculate_LongStringToCalculate_True()
+        {
+            Assert.AreEqual(true, calculator.IsCorrectStringToCalculate("96+2.39-78875/2"));
+        }
+
+        [TestMethod()]
+        public void IsZeroAfterOperation_StringIsOne_False()
+        {
+            Assert.AreEqual(false, calculator.IsZeroAfterOperation("1"));
+        }
+
+        [TestMethod()]
+        public void IsZeroAfterOperation_SumOfNumbers_False()
+        {
+            Assert.AreEqual(false, calculator.IsZeroAfterOperation("45+99"));
+        }
+
+        [TestMethod()]
+        public void IsZeroAfterOperation_LongStringToCalculate_True()
+        {
+            Assert.AreEqual(true, calculator.IsZeroAfterOperation("21/7+0"));
+        }
+
+        [TestMethod()]
+        public void StringToCalculateIsMinusZero_CommonString_False()
+        {
+            Assert.AreEqual(false, calculator.StringToCalculateIsMinusZero("21/7+0"));
+        }
+
+        [TestMethod()]
+        public void StringToCalculateIsMinusZero_MinusZero_True()
+        {
+            Assert.AreEqual(true, calculator.StringToCalculateIsMinusZero("-0"));
+        }
+
+        [TestMethod()]
+        public void StringToCalculateIsZero_CommonString_False()
+        {
+            Assert.AreEqual(false, calculator.StringToCalculateIsZero("21/7+0"));
+        }
+
+        [TestMethod()]
+        public void StringToCalculateIsZero_Zero_True()
+        {
+            Assert.AreEqual(true, calculator.StringToCalculateIsZero("0"));
+        }
+    }
+}
